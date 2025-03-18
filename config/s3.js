822b6d0f32cc2cf50424251cf791aa09c89cd485
m-d-webdev <mstph.iderkaoui@gmail.com>
2025-03-18 01:35:10 +0100

    incorrect import statement resolve on s3.js

--- config/s3.js
+++ config/s3.js
@@ -1,13 +1,15 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import dotenv from "dotenv"
+const { GetObjectCommand, PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const dotenv = require("dotenv")
 dotenv.config();
 
 
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
 const AWS_REGION = process.env.AWS_REGION
-const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET
+const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
+
+
 const s3 = new S3Client({
     credentials: {
         secretAccessKey: AWS_SECRET_ACCESS_KEY,
@@ -57,7 +59,7 @@ export const GET_FILE_URL_FROM_S3 = ({ key }) => {
                 });
                 const url = await getSignedUrl(s2, command, { expiresIn: "3600" })
                 resolve(url);
-                
+
             } catch (error) {
                 reject(error)
             }
